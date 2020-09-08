8803046d058c3ef4a7c4f045d429bb0841c357f8
Jason Easterly <jason.easterly11@gmail.com>
2020-09-08 22:35:14 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -34,7 +34,7 @@ clearanced_items = []
   if clearance_items[:clearance] == true
     clearance_items[:price] = clearance_items[:price] * 0.8.round(2)
   #  clearanced_items << clearance_items
-binding.pry
+
   end
   clearanced_items << clearance_items
 end
