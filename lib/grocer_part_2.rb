0456b38dbdabe394164743f86beb26e20617aa2b
Jason Easterly <jason.easterly11@gmail.com>
2020-09-08 20:11:28 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -9,19 +9,19 @@ binding.pry
   coupons.each do |coupon|
     item = find_item_by_name_in_collection(coupon[:item], cart)
     item_with_coupon = "#{coupon[:item]} W/COUPON"
-    item_has_coupon? = find_item_by_name_in_collection(item_with_coupon, cart)
+    item_has_coupon = find_item_by_name_in_collection(item_with_coupon, cart)
       if item && item[:count] >= coupon[:num]
-        if item_has_coupon?
-          item_has_coupon?[:count] += coupon[:num]
+        if item_has_coupon
+          item_has_coupon[:count] += coupon[:num]
           item[:count] -= coupon[:num]
         else
-          item_has_coupon? ={
+          item_has_coupon ={
             :item => item_with_coupon,
             :price => coupon[:cost] / coupon[:num],
             :count = coupon[:num],
             :clearance => item[:clearance]
           }
-        cart << item_has_coupon?
+        cart << item_has_coupon
         item[:count] -= coupon[:num]
         end
       end  
