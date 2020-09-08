828b68a4eda916d368e0af823b9ff8b47e6ce5bb
Jason Easterly <jason.easterly11@gmail.com>
2020-09-08 22:52:22 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -41,8 +41,8 @@ end
 
 def checkout(cart, coupons)
   consolidate_cart(cart)
-  binding.pry
   apply_coupons(cart, coupons)
+binding.pry  
   apply_clearance(cart)
 
   total = cart[:price] * cart[:count]
